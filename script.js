d148784adb3b348e85def9b275f854af4473dd43
kirill202020 <91893674+kirill202020@users.noreply.github.com>
2022-11-04 17:31:44 +0100

    final version

--- script.js
+++ script.js
@@ -33,6 +33,7 @@ btnAdd.addEventListener('click', () =>{
         task.unshift(new TaskObj(taskValue))
         addLocal()
         fillList()
+        updateCount()
     }
     input.value = ''
 })
@@ -80,7 +81,7 @@ const taskDone = index =>{
 const createCompleteTask = (task, index) =>{
     return`
     <div class="task__card task__done">
-        <button class="task__complete completed"></button>
+        <button class="task__complete completed" onclick='notDoneTask(${index})'></button>
         <div class="task__text">${task.text}</div>
         <button class="task__delete" onclick='deleteCompleteTask(${index})'></button>
     </div>
@@ -108,29 +109,29 @@ const deleteCompleteTask = index =>{
 const updateData = () =>{
     fillList()
     fillCompleteList()
+    updateCount()
 }
 
 
 
 
-updateData()
 
-
-
-const createUncompleteTask = (task, index) =>{
-    return`
-    <div class="task__card">
-        <button class="task__complete" onclick='taskUnDone(${index})></button>
-        <div class="task__text">${task.text}</div>
-        <button class="task__delete" onclick='deleteCompleteTask(${index})'></button>
-    </div>
-    `
-}
-
-
-const taskUnDone = index =>{
+const notDoneTask = index =>{
+    task.push(completeTask[index])
     completeTask.splice(index, 1)
     addLocal()
     updateData()
 }
+
+
 updateData()
+
+
+function updateCount(){
+    const allTask = document.querySelector('#all__task')
+    const completeCount = document.querySelector('#completed__task')
+
+    allTask.innerHTML = task.length + completeTask.length
+    completeCount.innerHTML = completeTask.length
+
+}
